7816d9d61712910d4e5aca904af2c9313547793a
rodims-code <ridrodims@hotmail.com>
2024-11-27 13:14:08 +0100

    Les differentes errer

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 import requests
 
-reponse = requests.get("https://www.google.com")
+url = input("Entrez l'url du site : ")
+requests.get(url)
 
-print(reponse.text)
+try:
+    reponse = requests.get(url)
+    reponse.raise_for_status()
+except requests.exceptions.HTTPError as errh :
+    print("Http Error : ", errh)
+except requests.exceptions.ConnectionError as errc :
+    print("Error connecting : ", errc )
+except requests.exceptions.Timeout as errt :
+    print("Timeout Error : ", errt)
+except requests.exceptions.RequestException as err :
+    print("OOps: Something Else", err)
+
+""" print(reponse.text) """
