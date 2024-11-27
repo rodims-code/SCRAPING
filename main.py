c4f81976538c362148431766d9ce8ffcb0e9ab2b
rodims-code <ridrodims@hotmail.com>
2024-11-27 13:25:54 +0100

    Creation du fichier html et ecriture

--- main.py
+++ main.py
@@ -1,18 +1,9 @@
 import requests
 
-url = input("Entrez l'url du site : ")
-requests.get(url)
+response = requests.get("https://www.google.com")
 
-try:
-    reponse = requests.get(url)
-    reponse.raise_for_status()
-except requests.exceptions.HTTPError as errh :
-    print("Http Error : ", errh)
-except requests.exceptions.ConnectionError as errc :
-    print("Error connecting : ", errc )
-except requests.exceptions.Timeout as errt :
-    print("Timeout Error : ", errt)
-except requests.exceptions.RequestException as err :
-    print("OOps: Something Else", err)
+with open("files_html/index.html", "w") as f :
+    f.write(response.text)
 
-""" print(reponse.text) """
+
+
