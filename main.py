49c32ded33589b76bab7042e37f74e8b7f0c7d85
rodims-code <ridrodims@hotmail.com>
2024-12-08 11:53:16 +0100

    Afficher tout les elements du body

--- main.py
+++ main.py
@@ -7,15 +7,7 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-# Fooction pour parcourrir recursivement l'arbre DOM
-def traver_dom(element, level=0) :
-    #Afficher l'element actuel
-    if element.name : 
-        print(f"{'' * level}<{element.name}>")
 
-    # Si l'element a des enfants, les parcourrir egalement
-    if hasattr(element, 'children') :
-        for child in element.children :
-            traver_dom(child, level + 1)
+body = soup.find_all('body')
 
-traver_dom(soup)
+print(body)
