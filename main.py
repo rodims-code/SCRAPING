cfdaec731fc899f1101cff0c0165298942a4c275
rodims-code <ridrodims@hotmail.com>
2024-12-08 11:24:42 +0100

    Boock.toscrip affichage sur le terminal avec soup.prettify()

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 
+url = "https://books.toscrape.com/"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, 'html.parser')
+
+print(soup.prettify())
